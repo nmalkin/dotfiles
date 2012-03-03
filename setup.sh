6268e64a6a4bbd72269d3c439bdbc6e02bfd2609
Nathan Malkin <nathan@nathanmalkin.com>
2012-03-03 10:43:49 -0500

    Include .pentadactylrc in setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ git submodule update
 
 mv -i ~/.vimrc ~/.vimrc.bak
 mv -i ~/.vim ~/.vim.bak
+mv -i ~/.pentadactylrc ~/.pentadactylrc 
 
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
+ln -s $PWD/.pentadactylrc ~/.pentadactylrc 
