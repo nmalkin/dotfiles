c766862475293cdd338b38e558069f28424a5d2f
Nathan Malkin <nathan@nathanmalkin.com>
2012-07-05 11:21:36 -0700

    Add gitconfig file

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ git submodule update
 mv -i ~/.vimrc ~/.vimrc.bak
 mv -i ~/.vim ~/.vim.bak
 mv -i ~/.pentadactylrc ~/.pentadactylrc.bak
+mv -i ~/.gitrc ~/.gitrc.bak
 
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.pentadactylrc ~/.pentadactylrc 
+ln -s $PWD/.gitrc ~/.gitrc
