15b68df617183fb249a6576e6ea9be872193917b
Nathan Malkin <nathan@nathanmalkin.com>
2012-07-18 16:36:41 -0700

    Add global .gitignore file

--- setup.sh
+++ setup.sh
@@ -7,8 +7,10 @@ mv -i ~/.vimrc ~/.vimrc.bak
 mv -i ~/.vim ~/.vim.bak
 mv -i ~/.pentadactylrc ~/.pentadactylrc.bak
 mv -i ~/.gitconfig ~/.gitconfig.bak
+mv -i ~/.gitignore_global ~/.gitignore_global.bak
 
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.pentadactylrc ~/.pentadactylrc 
 ln -s $PWD/.gitconfig ~/.gitconfig
+ln -s $PWD/.gitignore_global ~/.gitignore_global
