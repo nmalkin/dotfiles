98396fd5b5c1a513e109654d84b6c98e9547e4a9
Nathan Malkin <nathan@nathanmalkin.com>
2014-10-08 15:05:12 -0700

    Set up Vundle as part of install script

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ do
     mv -i ~/$file ~/$file.bak
     ln -s $PWD/$file ~/$file
 done
+
+# Get Vundle, then use it to install all other vim plugins
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
