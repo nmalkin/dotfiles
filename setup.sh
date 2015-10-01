75b93c6531f21e9a26e347440a38d04ffbe77e0c
Nathan Malkin <nathan@nathanmalkin.com>
2015-10-01 12:11:21 -0700

    Improve installation of auxiliary tools

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ do
     ln -s $PWD/$file ~/$file
 done
 
+# Install z.sh
+git clone https://github.com/rupa/z.git
+
 # Get Vundle, then use it to install all other vim plugins
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
