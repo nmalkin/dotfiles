a457eb45ac2659b7b50eb1b82fdf5088df74bea5
Nathan Malkin <nathan@nathanmalkin.com>
2016-03-28 17:57:11 -0700

    Commit Leiningen profile

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[venantius/ultra "0.4.1"]
+                  [lein-cljfmt "0.4.1"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-bikeshed "0.2.0"]
+                  [lein-kibit "0.1.2"]]
+        :dependencies [[lein-cljfmt "0.4.1"]]}}
