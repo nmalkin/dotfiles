a61d7bb2093e6d5ae346e33df818661717523cf3
Nathan Malkin <nathan@nathanmalkin.com>
2016-04-02 12:51:29 -0700

    Add lein-ancient and update plugins

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[venantius/ultra "0.4.1"]
-                  [lein-cljfmt "0.4.1"]
+                  [lein-cljfmt "0.5.0"]
                   [jonase/eastwood "0.2.3"]
-                  [lein-bikeshed "0.2.0"]
-                  [lein-kibit "0.1.2"]]
-        :dependencies [[lein-cljfmt "0.4.1"]]}}
+                  [lein-bikeshed "0.3.0"]
+                  [lein-kibit "0.1.2"]
+                  [lein-ancient "0.6.9"]]
+        :dependencies [[lein-cljfmt "0.5.0"]]}}
