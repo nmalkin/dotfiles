16c7e9c9ceef8a35fa9dceeedc6f46360dbf30bd
Nathan Malkin <nathan@nathanmalkin.com>
2016-06-08 10:48:22 -0700

    Provide explicit steps for setting up neovim

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ do
     ln -s $PWD/$file ~/$file
 done
 
+# Set up neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
 # Install z.sh
 git clone https://github.com/rupa/z.git
 
